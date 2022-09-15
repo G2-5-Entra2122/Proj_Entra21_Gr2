96c865f4f22ffa26424f49d0b25f0c9710156cc0
Felipe Sartorato <f_sartorato@outlook.com>
2022-09-15 09:10:17 -0300

    create model vagas

--- vagas/models.py
+++ vagas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Vaga(models.Model):
+    nome=models.CharField('Nome da Vaga',max_length=100)
+    empregador=models.CharField('Empregador',max_length=100)
+    niveis=[
+        'Junior',
+        'Pleno',
+        'Sênior'
+    ]
+    nivel=models.CharField('Nível da Vaga',choices=niveis)
+    descricao=models.CharField('Descrição da vaga')
+    modalidades=[
+        'Presencial',
+        'Híbrido',
+        'Remoto'
+    ]
+    tipo_contrato=models.CharField('Modalidade da Vaga',choices=modalidades)
+    local=models.CharField('Local da Vaga')
+    sncheck=[
+        'Sim',
+        'Não'
+    ]
+    outras_reg=models.CharField('Aceita candidatos de outras regiões?',choices=sncheck)
+    requisitos=models.CharField('Requisitos')
+    habil_obr=models.CharField('Habilidade')
+    salmin=models.DecimalField('Salário Mínimo',decimal_places=2)
+    salmax=models.DecimalField('Salário Máximo',decimal_places=2)
+    beneficios=models.CharField('Benefícios')
