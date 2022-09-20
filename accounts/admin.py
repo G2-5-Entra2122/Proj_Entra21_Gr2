86d4f4b96e25968ad85541aa18137272c484e8ee
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-09-20 16:35:37 -0300

    feat: Criado a adição de group Candidato e Empresa, com adição dos usuarios aos seus respectivos grupos

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import PerfilCandidatos
+from .models import PerfilCandidatos, PerfilEmpresas
 
-admin.site.register(PerfilCandidatos)
+@admin.register(PerfilCandidatos)
+class PerfilCandidatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'data_nasc')
+
+@admin.register(PerfilEmpresas)
+class PerfilEmpresasAdmin(admin.ModelAdmin):
+    list_display = ('fantasia', 'razao_social', 'tamanho', 'ramo')
