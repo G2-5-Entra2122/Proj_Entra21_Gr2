30a3c3c78448607985e322c3f9c6db85be92bb52
Felipe Sartorato <f_sartorato@outlook.com>
2022-09-22 11:17:51 -0300

    create forms & views

--- vagas/forms.py
+++ vagas/forms.py
@@ -1,1 +1,22 @@
-"teste"
+from django import forms
+from models import Vaga
+
+# form
+class VagasForm(forms.ModelForm):
+
+    class Meta:
+        model=Vaga
+        fields=[
+            'nome',
+            'nivel',
+            'descricao',
+            'tipo_contrato',
+            'local',
+            'outras_reg',
+            'requisitos',
+            'habil_obr',
+            'salmin',
+            'salmax',
+            'beneficios'
+        ]
+
--- vagas/views.py
+++ vagas/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from models import Vaga
+from forms import VagasForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def vaga_add(request):
+    if request.method=='POST':
+        form=VagasForm(request.POST)
+        if form.is_valid():
+            nova_vaga=form.save()
+            nova_vaga.save()
+            return HttpResponseRedirect('/vagas.html') # Nome provisório. A ser visto e alterado.
+    else:
+        form=VagasForm()
+
+    vagas=Vaga.objects.all()
+
+    return render(request,'vagas/add.html',
+    {
+        'form':form,
+        'vagas':vagas
+    }
+    )
