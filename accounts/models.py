d896747a302db274d0e7a9c81583d182c1b33f43
Felipe Sartorato <f_sartorato@outlook.com>
2022-10-07 11:53:54 -0300

    add help_text candidatos/empresas

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,11 @@ class PerfilCandidatos(models.Model):
     cpf = models.CharField('CPF', max_length=14, null=True)
     cep = models.CharField('CEP', max_length=9, null=True)
     data_nasc = models.DateField('Data de Nascimento', max_length=10 , null=True)
-    github = models.CharField('GitHub', max_length=100, default='#')
-    linkedin = models.CharField('Linkedin', max_length=100, default='#')
-    facebook = models.CharField('Facebook', max_length=100, default='#')
-    instagram = models.CharField('Instagram', max_length=100, default='#')
-    descricao = models.TextField('Descrição', max_length=200, null=True)
+    github = models.CharField('GitHub', max_length=100, default='#',help_text='URL do seu perfil no GitHub')
+    linkedin = models.CharField('Linkedin', max_length=100, default='#',help_text='URL do seu perfil no LinkedIn')
+    facebook = models.CharField('Facebook', max_length=100, default='#',help_text='URL do seu perfil no Facebook')
+    instagram = models.CharField('Instagram', max_length=100, default='#',help_text='URL do seu perfil no Instagram')
+    descricao = models.TextField('Descrição', max_length=300, null=True,help_text='Resumo do seu perfil profissional em no máximo 300 caracteres.')
     usuario = models.OneToOneField(User, on_delete=models.PROTECT)
 
     class Meta:
@@ -23,10 +23,10 @@ class PerfilCandidatos(models.Model):
         return self.nome
 
 class PerfilEmpresas(models.Model):
-    fantasia = models.CharField('Nome Fantasia', max_length=100, null=True)
-    razao_social = models.CharField('Razão Social', max_length=100, null=True)
-    cnpj = models.CharField('CNPJ', max_length=18, null=True)
-    ie = models.CharField('Inscrição Estadual', max_length=20, null=True, blank=True)
+    fantasia = models.CharField('Nome Fantasia', max_length=100, null=True,help_text='Nome fantasia da sua empresa')
+    razao_social = models.CharField('Razão Social', max_length=100, null=True,help_text='Razão social da sua empresa')
+    cnpj = models.CharField('CNPJ', max_length=18, null=True,help_text='Apenas números')
+    ie = models.CharField('Inscrição Estadual', max_length=20, null=True, blank=True,help_text='Inscrição estadual da sua empresa')
     endereco = models.CharField('Endereço', max_length=100, null=True)
     
     TAMANHO_CHOICES = (
@@ -47,9 +47,9 @@ class PerfilEmpresas(models.Model):
         ('comercio', 'Comércio')
     )
     ramo = models.CharField('Ramo', max_length=50, choices=RAMO_CHOICES, null=True)
-    cep = models.CharField('CEP', max_length=9, null=True)
-    telefone = models.CharField('Telefone', max_length=15, null=True)
-    apresentacao = models.TextField('Apresentação', max_length=500)
+    cep = models.CharField('CEP', max_length=9, null=True,help_text='Apenas números')
+    telefone = models.CharField('Telefone', max_length=15, null=True,help_text='Apenas números, com prefixo DDD em 2 dígitos')
+    apresentacao = models.TextField('Apresentação', max_length=500,help_text='Apresente sua empresa em no máximo 500 caracteres')
     usuario = models.OneToOneField(User, on_delete=models.PROTECT)
     #logo = models.ImageField('Logo') limitar o tamanho da logo
 
