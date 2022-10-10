5baf898e9a1b15b8553eeaac60b65793e47c9875
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-10-10 11:59:39 -0300

    Erro urls app

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, SobreView, VagasView, EmpresasView
+from .views import HomeView, SobreView, EmpresasView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    path('vagas', VagasView.as_view(), name='vagas'),
     path('empresas', EmpresasView.as_view(), name='empresas'),
     path('about', SobreView.as_view(), name= 'about')
 ]
