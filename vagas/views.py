0ccd38b0b4ef5f6fb2592d7224ef373bb33c669a
JOÃO PAULO CORREA SANTINI <joaopaulosantini@gmail.com>
2022-10-14 10:02:29 -0300

    paginação e url replace

--- vagas/views.py
+++ vagas/views.py
@@ -100,6 +100,7 @@ class MinhasVagasListView(GroupRequiredMixin, LoginRequiredMixin, ListView):
 class VagaListView(ListView):
     template_name='vagas/vagas-list.html'
     model = Vaga
+    paginate_by: 4
 
 
 
