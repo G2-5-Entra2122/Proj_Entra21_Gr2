30544ef1be93ac4c1ef6697267d71fb03bec475f
Luiza Bicalho Pollmann <104363623+luizabpollmann@users.noreply.github.com>
2022-10-15 19:07:45 -0300

    ajustes no urls e views sobre pagia empresas-list

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, SobreView, EmpresasView, ErrorView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    path('empresas', EmpresasView.as_view(), name='empresas'),
+    path('empresas-lista', EmpresasView.as_view(), name='empresas'),
     path('about', SobreView.as_view(), name= 'about'),
     path('about', ErrorView.as_view(), name= '404')
 ]
