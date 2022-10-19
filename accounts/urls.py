e9500a39e51d42b18fbaa7be657f914b99954c71
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-10-18 22:45:57 -0300

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,10 +9,13 @@ urlpatterns = [
             template_name='registration/login.html'
         ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('registrar/candidato', CandidatoCreate.as_view(), name='registrar-candidato'),
     path('registrar/empresa', EmpresaCreate.as_view(), name='registrar-empresa'),
-    path('atualizar-dados/candidato/', PerfilCandidatoUpdateView.as_view(), name='atualizar-dados'),
+
+    path('atualizar-dados/candidato/', PerfilCandidatoUpdateView.as_view(), name='atualizar-dados-candidato'),
     path('atualizar-dados/empresa/', PerfilEmpresaUpdateView.as_view(), name='atualizar-dados-empresas'),
+
     path('alterar-senha', views.change_password, name='change-password') 
 
 ]
