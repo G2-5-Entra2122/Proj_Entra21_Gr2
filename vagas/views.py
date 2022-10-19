f8bdc04afbbf308e510c8087084c25f756c7c641
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-10-19 03:02:29 -0300

    feat: Forms Empresa create/update vagas

--- vagas/views.py
+++ vagas/views.py
@@ -1,3 +1,4 @@
+from .forms import VagaForm
 from .models import Vaga
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -18,29 +19,7 @@ from django.shortcuts import get_object_or_404
 class VagaCreateView(GroupRequiredMixin, LoginRequiredMixin, CreateView):
     group_required= u'Empresa'
     login_url=reverse_lazy('login')
-    model = Vaga
-    fields=[
-            'nome',
-            'categoria',
-            'nivel',
-            'descricao',
-            'modalidade',
-            'contrato',
-            'jornada',
-            'local',
-            'outras_reg',
-            'requisitos',
-            'salmin',
-            'salmax',
-            'beneficios',
-            'pri_habilidade_vaga',
-            'seg_habilidade_vaga',
-            'ter_habilidade_vaga',
-            'qua_habilidade_vaga',
-            'qui_habilidade_vaga',
-        ]
-    # form_class=VagasForm
-    # vagas_obj=Vaga.objects.all()
+    form_class = VagaForm
     template_name = 'vagas/form.html'
     success_url=reverse_lazy('minhas-vagas')
 
@@ -114,27 +93,8 @@ class VagaExibirListView(ListView):
 class VagaUpdateView(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
     group_required = u'Empresa'
     login_url = reverse_lazy('login')
-    model = Vaga
-    fields=[
-            'nome',
-            'categoria',
-            'nivel',
-            'descricao',
-            'modalidade',
-            'contrato',
-            'jornada',
-            'local',
-            'outras_reg',
-            'requisitos',
-            'salmin',
-            'salmax',
-            'beneficios',
-            'pri_habilidade_vaga',
-            'seg_habilidade_vaga',
-            'ter_habilidade_vaga',
-            'qua_habilidade_vaga',
-            'qui_habilidade_vaga',
-        ]
+    form_class = VagaForm
+  
     template_name = 'vagas/form.html'
     success_url = reverse_lazy('minhas-vagas')
 
