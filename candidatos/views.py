91edf6919177f7aa3e9015e37ee07adaa7adbb36
Nicolas Felipe da Silva <56174366+Nicolas-FelSi@users.noreply.github.com>
2022-10-20 11:59:12 -0300

    PRO-76 - add contador candidatos (#55)
    * add contador e filtros página candidatos
    
    * estilização botão filtrar e correção textos

--- candidatos/views.py
+++ candidatos/views.py
@@ -10,7 +10,7 @@ from django.views.generic.detail import DetailView
 from .models import Curriculo
 from .forms import CurriculoForm
 
-
+from vagas.forms import FilterForm
 
 
 
@@ -58,6 +58,37 @@ class CurriculosUpdateView(LoginRequiredMixin, UpdateView):
 class CandidatoListView(ListView):
     model = Curriculo
     template_name = 'candidatos/candidatos-list.html'
+
+    def get_queryset(self):
+        candidatos = Curriculo.objects.all()
+        search_001 = self.request.GET.get('src01')
+        if search_001:
+            if search_001 != "todas-as-categorias":
+                candidatos = candidatos.filter(categoria=search_001)
+
+        search_002 = self.request.GET.get('src02')
+        if search_002:
+            if search_002 != "todas-as-modalidades":
+                candidatos = candidatos.filter(modalidade=search_002)
+
+        search_003 = self.request.GET.get('src03')
+        if search_003:
+            if search_003 != "tipo-de-contrato":
+                candidatos = candidatos.filter(contrato=search_003)
+
+        
+        self.contador = candidatos.count()
+
+        return candidatos
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)    
+        context['contador']  = self.contador 
+        context['form'] = FilterForm(initial={
+            'search': self.request.GET.get('search', ''),
+            'filter_field': self.request.GET.get('filter_field', '')
+        })
+        return context
 
 class CandidatoPerfilView(DetailView):
     model = Curriculo
